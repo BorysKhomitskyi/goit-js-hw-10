8df91e86b70bd321a42cfb3883185cca9db9b7dc
BorysKhomitskyi <borys2130@gmail.com>
2023-02-08 22:27:31 +0200

    js-hw10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 import './css/styles.css';
 
+import { fetchCountries } from './js/fetchCountries';
+import { refs } from './js/refs-elem';
+import { countryСard, countryList } from './js/markup';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+
 const DEBOUNCE_DELAY = 300;
+refs.searchBox.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+
+
+function onInputSearch(evt) {
+    const searchParams = refs.searchBox.value.trim();
+    if (searchParams === '') {
+        refs.countryInfo.innerHTML = '';
+        refs.countryList.innerHTML = '';
+        return;
+    }
+
+    fetchCountries(searchParams)
+        .then(countrys => {
+            if (countrys.length > 10) {
+                Notify.info('Too many matches found. Please enter a more specific name.');
+                refs.countryInfo.innerHTML = '';
+                refs.countryList.innerHTML = '';
+                return;
+            }
+
+            if (countrys.length <= 10) {
+                const listMarkup = countrys.map(country => countryList(country));
+                refs.countryList.innerHTML = listMarkup.join('');
+                refs.countryInfo.innerHTML = '';
+            }
+
+            if (countrys.length === 1) {
+                const markup = countrys.map(country => countryСard(country));
+                refs.countryInfo.innerHTML = markup.join('');
+                refs.countryList.innerHTML = '';
+            }
+        })
+        .catch(error => {
+            Notify.failure('Oops, there is no country with that name');
+            refs.countryInfo.innerHTML = '';
+            refs.countryList.innerHTML = '';
+            return error;
+        });
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,13 @@
+export function fetchCountries(name) {
+    const url = `https://restcountries.com/v3.1/name/`;
+    const filters = '?fields=name,capital,population,flags,languages';
+
+    return fetch(`${url}${name}${filters}`).then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+
+        return response.json();
+    })
+    .catch(error => console.log(`${error.name}: ${error.message}`));
+}
